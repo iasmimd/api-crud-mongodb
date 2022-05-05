808fef6ba3b3975d394c0e321ec83630d7bc1257
iasmim <iasmimstuff@gmail.com>
2022-05-05 16:06:18 -0300

    fix: call config from dot env

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-import { config } from "dotenv";
 import express from "express";
+import { config } from "dotenv";
 import userRouter from "./routes/user.js";
 
 config()
-
 const app = express()
 
 app.use(express.json())
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import app from "./app.js"
 
-app.listen(3000, () => console.log("server is running"))
+app.listen(process.env.PORT || 3000, () => console.log("server is running"))
