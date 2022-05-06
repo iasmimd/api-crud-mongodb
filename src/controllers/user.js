f929c2f45f46ba551941281dc8e98a49a9435b3d
iasmim <iasmimstuff@gmail.com>
2022-05-06 16:38:05 -0300

    feat: delete request

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -28,6 +28,47 @@ class UserControllers {
             return res.status(500).json({ "error": "algo deu errado" })
         }
     }
+
+    static async findOne(req, res) {
+        try {
+            const { id } = req.params
+            const users = await User.findById(id)
+            return res.json(users)
+        } catch (error) {
+            return res.status(500).json({ "error": "algo deu errado" })
+        }
+    }
+
+    static async updateUser(req, res) {
+        try {
+            const { id } = req.params
+            const { password, avatarUrl } = req.body
+            const userUpdated = await User.findByIdAndUpdate(id, {
+                 password, avatarUrl, new: true
+            })
+
+            res.status(200).json(userUpdated)
+
+        } catch (error) {
+            res.status(500).json({ "error": "algo deu errado ):" })
+            console.log(error)
+        }
+    }
+
+    static async deleteUser(req, res){
+        try {
+            const {id} = req.params
+
+            await User.findByIdAndRemove(id)
+
+            res.status(204).json({})
+        } catch (error) {
+            res.status(500).json({ "error": "algo deu errado ):" })
+            console.log(error)
+        }
+    }
+
+
 }
 
 export default UserControllers
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,5 +5,8 @@ const userRouter = Router()
 
 userRouter.post("", (req, res) => UserControllers.createUser(req, res))
 userRouter.get("", (req, res) => UserControllers.findAll(req, res))
+userRouter.get("/:id", (req, res) => UserControllers.findOne(req, res))
+userRouter.patch("/:id", (req, res) => UserControllers.updateUser(req, res))
+userRouter.delete("/:id", (req, res) => UserControllers.deleteUser(req, res))
 
 export default userRouter
